391d8399087bba0d00be7ca77958606a5709bdf1
vlievin <valentin.lievin@gmail.com>
2015-11-25 18:40:39 +0100

    add function from izi.py to up.py

--- up.py
+++ up.py
@@ -12,12 +12,63 @@ import json
 from flask.ext.pymongo import PyMongo
 
 from pymongo import MongoClient
+import pickle
 
 client = MongoClient()
 db = client.izidb
 
 
-from scripts import izi
+min_score = 0.04
+
+colours = []
+colours.append( "#7f8c8d")
+colours.append( "#e74c3c")
+colours.append( "#3498db")
+colours.append( "#f1c40f")
+colours.append( "#9b59b6")
+colours.append( "#1abc9c")
+colours.append( "#34495e")
+colours.append( "#e67e22")
+
+
+class idGenerator:
+    def __init__(self):
+        self.id = 0
+    def get(self):
+        self.id += 1
+        return self.id - 1
+
+def getTopTopics( vec ):
+    topic_names = pickle.load( open( 'scripts/' +  "topics_names.p", "rb" ) )
+    pairs = []
+    result = []
+    for i in range(0, len(vec)):
+        pairs.append( (i , vec[i] ) )
+    k = 1
+    s = 0.0
+    for i in sorted(pairs, key=lambda tup: tup[1], reverse = True):
+        if i[1] > min_score and k < len(colours):
+            tmp = dict()
+            tmp[ 'value' ] = i[1] 
+            s += i[1]
+            tmp['name'] = topic_names[i[0]]
+            tmp['color'] = colours[k]
+            result.append( tmp )
+            k += 1
+            # if k >= len(colours):
+            #   k = 0
+
+    others = dict()
+    others[ 'value' ] = 1.0 - s
+    others['name'] = 'others'
+    others['color'] = 'lightgray'
+    result.append(others)
+
+    return result
+
+
+
+# from scripts import izi
 
 
 app = Flask(__name__)
@@ -125,12 +176,13 @@ def getTopics(title):
     if txt:
         data = dict()
         vec = txt['semantic_vec']
-        topics = izi.getTopTopics(vec)
+        topics = getTopTopics(vec)
         data['topics'] = topics
         return json.dumps(data)
     else:
         return "failed to get text"
 
+@app.route('/', methods=['GET', 'POST'])
 @app.route("/network", methods=['GET', 'POST'])
 def network():
 	SIMILARITY_CUTOFF = 0.85
@@ -141,7 +193,7 @@ def network():
 	current_id = lasDoc["_id"]
 
 	id2db = dict()
-	gen = izi.idGenerator()
+	gen = idGenerator()
 
 	nodes = []
 	for ii in list_ids:
@@ -190,33 +242,33 @@ def is_image(nomfic):
     return '.' in nomfic and nomfic.rsplit('.', 1)[1] in ('jpg', 'png')
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/up/', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # if request.form['pw'] == 'up': # on vérifie que le mot de passe est bon
-        f = request.files['fic']
-        if f: # on vérifie qu'un fichier a bien été envoyé
-            if extension_ok(f.filename): # on vérifie que son extension est valide
-                nom = secure_filename(f.filename)
-                # remove previous files
-                images = [img for img in os.listdir(DOSSIER_UPS)]
-                for i in images:
-                    os.remove(DOSSIER_UPS + i)
-                txts = [t for t in os.listdir(TEXT_FOLDERS)]
-                for i in txts:
-                    os.remove(TEXT_FOLDERS + i)
-                f.save(TEXT_FOLDERS + nom)
-                insertDoc(TEXT_FOLDERS + nom)
-                # processFile( TEXT_FOLDERS + nom )
-                flash(u'File sent. Here is the <a href="{lien}"> link </a>.'.format(lien=url_for('d3')), 'succes')
-            else:
-                flash(u'Ce fichier ne porte pas une extension autorisée !', 'error')
-        else:
-            flash(u'Vous avez oublié le fichier !', 'error')
-        # else:
-        #     flash(u'Mot de passe incorrect', 'error')
-    return render_template('up_up.html')
+# @app.route('/', methods=['GET', 'POST'])
+# @app.route('/up/', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         # if request.form['pw'] == 'up': # on vérifie que le mot de passe est bon
+#         f = request.files['fic']
+#         if f: # on vérifie qu'un fichier a bien été envoyé
+#             if extension_ok(f.filename): # on vérifie que son extension est valide
+#                 nom = secure_filename(f.filename)
+#                 # remove previous files
+#                 images = [img for img in os.listdir(DOSSIER_UPS)]
+#                 for i in images:
+#                     os.remove(DOSSIER_UPS + i)
+#                 txts = [t for t in os.listdir(TEXT_FOLDERS)]
+#                 for i in txts:
+#                     os.remove(TEXT_FOLDERS + i)
+#                 f.save(TEXT_FOLDERS + nom)
+#                 insertDoc(TEXT_FOLDERS + nom)
+#                 # processFile( TEXT_FOLDERS + nom )
+#                 flash(u'File sent. Here is the <a href="{lien}"> link </a>.'.format(lien=url_for('d3')), 'succes')
+#             else:
+#                 flash(u'Ce fichier ne porte pas une extension autorisée !', 'error')
+#         else:
+#             flash(u'Vous avez oublié le fichier !', 'error')
+#         # else:
+#         #     flash(u'Mot de passe incorrect', 'error')
+#     return render_template('up_up.html')
 
 @app.route('/up/view/', methods=['GET', 'POST'])
 def liste_upped():
